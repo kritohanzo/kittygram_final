7850ae753683ef148c780a5cb597be221c9d7997
Vagif Aliev <kritohanzo0@gmail.com>
2023-06-27 18:32:48 +0500

    add sh script

--- backend/migrate_collect_static.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python manage.py migrate
-python manage.py collectstatic
-cp -r /app/collected_static/. /backend_static/static/
---
+++ backend/migrate_collect_static_gunicorn.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python manage.py migrate
+python manage.py collectstatic
+cp -r /app/collected_static/. /backend_static/static/
+gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
