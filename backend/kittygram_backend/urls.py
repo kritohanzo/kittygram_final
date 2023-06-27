bbeb88b9b137e0976c58ab7f224b2e523b255340
Vagif Aliev <kritohanzo0@gmail.com>
2023-06-28 00:10:30 +0500

    fix isort errors

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,10 +1,11 @@
-from cats.views import AchievementViewSet, CatViewSet
 from rest_framework import routers
 
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+from cats.views import AchievementViewSet, CatViewSet
 
 
 router = routers.DefaultRouter()
